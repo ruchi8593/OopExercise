fb991e1fb6a9a9408fa5f9ef86ab7a5e91026779
Niruxaben Padhiyar <nilipadhiyar293@outlook.com>
2022-01-15 15:27:49 -0600

    serialization

---
+++ serlization.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+/// <summary>
+/// Summary description for Class1
+/// </summary>
+/// 
+[Serializable]
+class Student
+{
+    int rollno;
+    string name;
+    public Student(int rollno, string name)
+    {
+        this.rollno = rollno;
+        this.name = name;
+    }
+}
+public class SerializeExample
+{
+    public static void Main(string[] args)
+    {
+        FileStream stream = new FileStream("e:\\sss.txt", FileMode.OpenOrCreate);
+        BinaryFormatter formatter = new BinaryFormatter();
+
+        Student s = new Student(101, "Nili");
+        formatter.Serialize(stream, s);
+
+        Student s = (Student)formatter.Deserialize(stream);
+        Console.WriteLine("Rollno: " + s.rollno);
+        Console.WriteLine("Name: " + s.name);
+
+
+        stream.Close();
+    }
+}
